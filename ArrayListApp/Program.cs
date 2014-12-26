8cf1e5af97066dcce4983d8cf632af8e15d3fe33
taukir4u <taukir4u@gmail.com>
2014-12-26 19:01:42 +0600

    C#
    Signed-off-by: taukir4u <taukir4u@gmail.com>

---
+++ ArrayListApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayListApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList newList = new ArrayList();
+            
+            Console.WriteLine("Size of ArrayList: " +newList.Count);
+
+            Console.WriteLine("Trying to add values... ");
+
+            newList.Add(5);
+
+            int count = newList.Count;
+
+            Console.WriteLine(newList[0]);
+
+            Console.ReadKey();
+        }
+    }
+}
